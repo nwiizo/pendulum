a9cabb8498616a7b74066c7116df02cd209e88cf
smotouchi <shuya-motouchi@gmo.jp>
2018-01-31 17:13:12 +0900

    shoukidehanai

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use std::env;
+use std::io;
 use std::thread;
 use std::process::Command;
 
 #[warn(deprecated)] 
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("My path is {}.", args[0]);
-    if args.len() == 3 {
-        loop{
-            thread::sleep_ms(600);
-            Command::new("sh").arg("-c").arg("ls").spawn().expect("failed to execute process");
-            }
-    }else{
-        println!("Error Args")
+    let mut guess = String::new();
+    println!("Please loop Command.");
+    io::stdin().read_line(&mut guess)
+        .ok()
+        .expect("Failed to read line");
+    println!("OK Loop {}",guess);
+
+    loop{
+        thread::sleep_ms(6000);
+        Command::new("sh").arg("-c").arg("date").spawn().expect("failed to execute process");
+        Command::new("sh").arg("-c").arg(&guess).spawn().expect("failed to execute process");
     }
 }
