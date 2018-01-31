81adafabd77ef13b07823e7c453a3d5f34e59a18
smotouchi <shuya-motouchi@gmo.jp>
2018-01-31 17:53:05 +0900

    fix args

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,21 @@ use std::process::Command;
 #[warn(deprecated)] 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("My path is {}.", args[0]);
+    println!("My path is {}. \n Please loop Commands:", args[0]);    
     let mut guess = String::new();
-    println!("Please loop Command.");
-    io::stdin().read_line(&mut guess)
-        .ok()
-        .expect("Failed to read line");
-    println!("OK Loop {}",guess);
 
+    if args.len() == 2 {
+        guess = args[1].clone();
+    } else {
+        io::stdin().read_line(&mut guess)
+            .ok()
+            .expect("Failed to read line");
+    }
+    
     loop{
-        thread::sleep_ms(6000);
+        println!("OK Loop {}",&guess);
         Command::new("sh").arg("-c").arg("date").spawn().expect("failed to execute process");
         Command::new("sh").arg("-c").arg(&guess).spawn().expect("failed to execute process");
+        thread::sleep_ms(6000);
     }
 }
