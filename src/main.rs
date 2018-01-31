963d00e65aef3c60b61c24e396bd9329ce51d03d
smotouchi <shuya-motouchi@gmo.jp>
2018-01-31 18:06:59 +0900

    ##### start #####

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,20 @@ use std::process::Command;
 #[warn(deprecated)] 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("My path is {}. \n Please loop Commands:", args[0]);    
     let mut guess = String::new();
 
     if args.len() == 2 {
         guess = args[1].clone();
     } else {
+        println!("Please loop Commands:");    
         io::stdin().read_line(&mut guess)
             .ok()
-            .expect("Failed to read line");
+            .expect("Failed to read line");   
+        println!("OK Loop {} \n ##### start #####",&guess);
     }
     
     loop{
-        println!("OK Loop {}",&guess);
-        Command::new("sh").arg("-c").arg("date").spawn().expect("failed to execute process");
+        Command::new("sh").arg("-c").arg("date +'%Y/%m/%d %I:%M:%S'").spawn().expect("failed to execute process");
         Command::new("sh").arg("-c").arg(&guess).spawn().expect("failed to execute process");
         thread::sleep_ms(6000);
     }
